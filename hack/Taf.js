5a9c6d41cf54bab699e8254c73100003bc05d9eb
Prince Kunga <147451253+Pktrill@users.noreply.github.com>
2024-08-09 14:14:51 +0100

    Add files via upload

---
+++ hack/Taf.js
@@ -0,0 +1,16 @@
+let typed = new Typed(".auto-type", {
+    strings: [
+      `Very beautiful Heels , Ultra High Heel Stiletto Sandals - Slip-On, Open-Toe, Solid Color, Comfortable, Faux Leather Insole, PVC Upper and Sole, Perfect for Nightclub Party and Dressy Occasions
+             wear with a red dress  it will absolutely lovely
+
+                `,
+    ],
+    typeSpeed: 30,
+    backSpeed: 30,
+    loop: true,
+  });
+  
+  let typed2 = new Typed(".auto-type2", {
+    strings: ["Stileto Heels"],
+    typeSpeed: 100,
+  });
